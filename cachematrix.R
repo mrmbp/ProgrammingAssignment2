af68dc606450448caf79ad92e135ba4d3e1ec066
mrmbp <mrmbp@yahoo.com>
2014-08-22 23:27:47 -0700

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions here, makeCacheMatrix and cacheSolve. The first function 
+#creates an invertible matrix and stores it and cacheSolve returns its mean.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates an invertible square matrix and uses the <<- operator to store its inverse.
 
+makeCacheMatrix <- function(x = matrix()) { #create matrix x
+        set <- function(y) {
+        x <<- y
+        m <<- NULL
+}
+get <- function() x
+setmatrix <- function(mean) m <<- mean 
+getmatrix <- function() m
+list(set = set, get = get,
+     setmatrix = setmatrix,
+     getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function checks the stored matrix to see if it has already been solved, in which case
+## it returns the solution; otherwise, it returns its inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        matrix <- x$get()
+        m <- solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
